fd7746d363650020701abc31adc53f9ec8af2e66
charank72 <kcharantheja72@gmail.com>
2024-10-03 20:57:57 +0530

    updated web

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,71 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png";
 import { Link } from "react-scroll";
+
 function Header() {
-  const mobile = window.innerWidth <= 768 ? true : false;
   const [menuOpened, setMenuOpened] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+      if (window.innerWidth > 768) {
+        setMenuOpened(false); // Close menu when resizing to desktop
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="header">
-      <img className="logo" src={Logo} alt="" />
-      {menuOpened === false && mobile === true ? (
-        <div>
-          <img
-            onClick={() => {
-              setMenuOpened(true);
-            }}
-            className="bars"
-            src={Bars}
-            alt=""
-          />
-        </div>
+      <img className="logo" src={Logo} alt="Logo" />
+      {!menuOpened && isMobile ? (
+        <img
+          onClick={() => setMenuOpened(true)}
+          className="bars"
+          src={Bars}
+          alt="Menu"
+        />
       ) : (
         <ul className="header-menu">
+          {["home", "programs", "reasons", "plans", "testimonials"].map(
+            item => (
+              <li key={item}>
+                <Link
+                  onClick={() => setMenuOpened(false)}
+                  to={item}
+                  span={true}
+                  smooth={true}
+                >
+                  {item.charAt(0).toUpperCase() + item.slice(1)}
+                </Link>
+              </li>
+            )
+          )}
           <li>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              to="home"
-              span={true}
-              smooth={true}
-            >
-              Home
-            </Link>
+            <button onClick={() => setMenuOpened(false)} className="close-menu">
+              Close
+            </button>
           </li>
-          <li>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              to="programs"
-              span={true}
-              smooth={true}
-            >
-              Programs
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              to="reasons"
-              span={true}
-              smooth={true}
-            >
-              why us
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              to="plans"
-              span={true}
-              smooth={true}
-            >
-              plans
-            </Link>
-          </li>
-          <li><Link onClick={() => setMenuOpened(false)} to="testimonials" span={true}
-            smooth={true}>
-              Testimonials
-            </Link></li>
         </ul>
       )}
     </div>
